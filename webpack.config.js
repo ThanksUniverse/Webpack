f3c57eae76f80e6ad9b9d09cc231ab76563c6390
Pedro <ThanksUniverse333@outlook.com>
2022-06-16 15:27:21 -0300

    Files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
-const path = require( 'path' )
+const path = require('path')
+const html = require('html-webpack-plugin')
 
 module.exports = {
    entry: "./src/index.js",
    output: {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist')
-   }
+   },
+   plugins: [
+      new html({
+         filename: 'index.html', //Nome do arquivo
+         template: './src/index.html' //Onde buscar o arquivo
+      })
+   ]
 }
