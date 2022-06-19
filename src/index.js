6af23ca60b92713f0c335db20d1ef0437fa43a83
Pedro <ThanksUniverse333@outlook.com>
2022-06-19 17:31:27 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import './styles.scss'
 
 function titleComponent() {
    const elemH1 = document.createElement('h1')
    elemH1.innerHTML = "Hello World Yeh!"
+   elemH1.classList.add('title')
    return elemH1
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const html = require('html-webpack-plugin')
+const miniCss = require('mini-css-extract-plugin')
 
 module.exports = {
    entry: "./src/index.js",
@@ -7,10 +8,32 @@ module.exports = {
       filename: 'main.js',
       path: path.resolve(__dirname, 'dist')
    },
+   module: {
+      rules: [
+         {
+            test: /\.(sa|c|sc)ss$/, //Expressão Regular -- Regex
+            use: [
+               miniCss.loader,
+               'css-loader',
+               "sass-loader"
+            ]
+         },
+         {
+            test: /\.css$/i,
+            use: [
+               'style-loader',
+               'css-loader'
+            ]
+         }
+      ]
+   },
    plugins: [
       new html({
          filename: 'index.html', //Nome do arquivo
          template: './src/index.html' //Onde buscar o arquivo
+      }),
+      new miniCss({
+         filename: 'styles.css'
       })
    ]
 }
